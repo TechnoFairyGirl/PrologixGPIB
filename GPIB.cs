0baef9dbe14f8511eeabe3c723d10e068345055d
Lily Finley <TechnoFairyGirl@users.noreply.github.com>
2022-03-26 06:21:33 -0500

    Initial code commit.

---
+++ GPIB.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrologixGPIB
+{
+	public class GPIB : IDisposable
+	{
+		public string Host { get; }
+		public int Address { get; }
+		public int Timeout { get; }
+
+		readonly TcpClient client;
+		readonly NetworkStream stream;
+
+		static string EscapeString(string str) => str
+			.Replace("\u001B", "\u001B\u001B")
+			.Replace("\r", "\u001B\r")
+			.Replace("\n", "\u001B\n")
+			.Replace("+", "\u001B+");
+
+		public GPIB(string host, int address, int timeout = 3000)
+		{
+			Host = host;
+			Address = address;
+			Timeout = timeout;
+
+			client = new TcpClient(host, 1234);
+			stream = client.GetStream();
+
+			stream.WriteLine("++savecfg 0");
+			stream.WriteLine("++mode 1");
+			stream.WriteLine($"++addr {address}");
+			stream.WriteLine("++auto 0");
+			stream.WriteLine("++eoi 1");
+			stream.WriteLine("++eos 2");
+			stream.WriteLine("++eot_enable 0");
+			stream.WriteLine("++eot_char 0");
+			stream.WriteLine($"++read_tmo_ms {timeout}");
+			stream.WriteLine("++ifc");
+		}
+
+		public void Send(string line) =>
+			stream.WriteLine(EscapeString(line));
+
+		public string ReceiveLine()
+		{
+			stream.WriteLine("++read eoi");
+			return stream.ReadLine();
+		}
+
+		public string Query(string query)
+		{
+			Send(query);
+			return ReceiveLine();
+		}
+
+		public byte[] ReceiveAllData(int? timeout = null)
+		{
+			stream.WriteLine("++read eoi");
+
+			var data = new List<byte>();
+
+			while (true)
+			{
+				Task.Delay(timeout ?? Timeout).Wait();
+				var dataAvailable = client.Available;
+				if (dataAvailable == 0) break;
+				var dataBlock = stream.ReadBytes(dataAvailable);
+				data.AddRange(dataBlock);
+			}
+
+			return data.ToArray();
+		}
+
+		public byte[] BinaryQuery(string query, int? timeout = null)
+		{
+			Send(query);
+			return ReceiveAllData(timeout);
+		}
+
+		public void Local() =>
+			stream.WriteLine("++loc");
+
+		public void Dispose()
+		{
+			stream.Dispose();
+			client.Dispose();
+		}
+	}
+}
---
+++ StreamExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrologixGPIB
+{
+	static class StreamExtensions
+	{
+		public static UTF8Encoding UTF8 { get; } = new UTF8Encoding(false);
+
+		public static void WriteLine(this Stream stream, string text)
+		{
+			using var writer = new StreamWriter(stream, UTF8, 4096, true);
+			writer.WriteLine(text);
+		}
+
+		public static string ReadLine(this Stream stream)
+		{
+			var reader = new StreamReader(stream, UTF8, false, 4096, true);
+			return reader.ReadLine();
+		}
+
+		public static long CopyBlockTo(this Stream inStream, Stream outStream, long length)
+		{
+			byte[] buffer = new byte[4096];
+			long bytesCopied = 0;
+
+			while (true)
+			{
+				var bytesRead = inStream.Read(buffer, 0, (int)Math.Min(length - bytesCopied, buffer.Length));
+				outStream.Write(buffer, 0, bytesRead);
+				bytesCopied += bytesRead;
+				if (bytesCopied >= length || bytesRead == 0)
+					break;
+			}
+
+			return bytesCopied;
+		}
+
+		public static byte[] ReadBytes(this Stream stream, long length)
+		{
+			using var buffer = new MemoryStream();
+			stream.CopyBlockTo(buffer, length);
+			return buffer.ToArray();
+		}
+	}
+}
